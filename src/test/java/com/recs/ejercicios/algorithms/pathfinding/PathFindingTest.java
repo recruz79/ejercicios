56274b24c7cc02f86f57d92e4553c4224451f002
recruz79 <raul.cruz79@gmail.com>
2022-03-11 12:01:55 -0300

    Refactored method examples

--- src/test/java/com/recs/ejercicios/algorithms/pathfinding/PathFindingTest.java
+++ src/test/java/com/recs/ejercicios/algorithms/pathfinding/PathFindingTest.java
@@ -2,6 +2,10 @@ package com.recs.ejercicios.algorithms.pathfinding;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 class PathFindingTest {
 
     PathFinding pathFinding = new PathFinding();
@@ -16,6 +20,25 @@ class PathFindingTest {
     public void testArcMinimunPath2() {
         OrientedArc orientedArc = pathFinding.calculateShortestPath(25, 10, 15);
         System.out.println("The returned Arc has the shortest distance with : " + orientedArc.length + " steps, clockswise: " + orientedArc.clockwise);
+    }
+
+    @Test
+    public void testCountTotalSteps() {
+        String word = "ARCO".toLowerCase();
+        List<Integer> listOfCharacters = word.chars().mapToObj(p -> p).collect(Collectors.toList());
+        System.out.println(listOfCharacters);
+        Integer start = 1;
+        Integer circumference = 26;
+        Integer totalSteps = 0;
+        for(Integer characterd : listOfCharacters) {
+            Integer positionedCharacter = (characterd - 96);
+            OrientedArc orientedArc = pathFinding.calculateShortestPath(circumference, start, positionedCharacter);
+            System.out.println("First step count : " + orientedArc.length + ", with values: start " + start + " character : " + positionedCharacter );
+            totalSteps += orientedArc.length;
+            start = positionedCharacter;
+        }
+
+        System.out.println("Total steps counted : " + totalSteps);
     }
 
 }
